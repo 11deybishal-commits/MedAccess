aa93b18566e39abe8aac847ca363fd1b1f3e9330
MedAccess Developer <contact@medaccess.com>
2026-03-16 18:41:59 +0530

    changed a bit

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
     }
   };
 } else {
-  corsOptions.origin = process.env.FRONTEND_URL || 'http://localhost:5183';
+  corsOptions.origin = process.env.FRONTEND_URL || 'http://localhost:5181';
 }
 
 app.use(cors(corsOptions));
